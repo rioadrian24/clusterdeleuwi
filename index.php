72d691a3aaae60f62353d7d6002b6118842dffa9
rioadrian24 <rioadrian646@gmail.com>
2021-11-17 16:32:40 +0700

    update

---
+++ index.php
@@ -0,0 +1,266 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="app.css">
+
+    <!-- Fresco -->
+    <link rel="stylesheet" href="/assets/vendor/fresco/dist/css/fresco.css">
+
+    <title>Cluster De'Leuwi | Leuwinanggung - Depok</title>
+</head>
+
+<body>
+    <div class="widget-circle shadow">
+        <div class="content text-center">
+            <h2 class="text-white mb-0">PROMO</h2>
+            <h2 class="text-white mb-0">AKHIR</h2>
+            <h2 class="text-white mb-0">Tahun</h2>
+        </div>
+    </div>
+
+    <div class="content-top">
+        <div class="component-1">
+            <img src="assets/img/component/silue.png">
+        </div>
+        <div class="component-2">
+            <img src="assets/img/component/galaxy.png">
+        </div>
+        <div class="logo-center py-5">
+            <img src="assets/img/component/logo.png" class="logo" alt="Logo Cluster Deleuwi">
+        </div>
+        <div class="component-3 py-5">
+            <img src="assets/img/component/miliki.png">
+        </div>
+    </div>
+
+    <div class="content-body">
+        <div class="section-1 py-6">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-6">
+                        <div class="home-1">
+                            <img src="assets/img/home/home1.jpeg">
+                        </div>
+                    </div>
+                    <div class="col-lg-6 text-center">
+                        <img src="assets/img/component/up.png" class="mb-4" width="90%">
+                        <div class="d-flex justify-content-center">
+                            <a href="https://wa.me/6281298830776" target="_blank" class="btn btn-whatsapp btn-lg rounded-pill shadow"><i class="bi bi-whatsapp"></i> Saya Tertarik</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="section-2">
+            <div class="home-2">
+                <img src="assets/img/home/home2.jpeg" width="100%">
+            </div>
+            <div class="specification">
+                <div class="spec-title mb-3">
+                    <h5 class="mb-0">Spesifikasi :</h5>
+                </div>
+                <ul class="spec-list">
+                    <li>Pondasi Batu Bata</li>
+                    <li>Plaster Luar Dalam + Cat</li>
+                    <li>Plafon Gypsum</li>
+                    <li>Pintu Panel Kayu</li>
+                    <li>Asesoris kunci pintu merek SOLID</li>
+                    <li>Full Keramik 40x40</li>
+                    <li>Kamar mandi shower & closet duduk</li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="section-3 py-6">
+            <div class="container">
+                <h1 class="title text-center mb-6">Promo akhir tahun</h1>
+                <div class="row align-items-center">
+                    <div class="col-lg-6">
+                        <img src="assets/img/component/teks.png" class="mb-3" width="100%">
+                        <div class="d-flex justify-content-center">
+                            <a href="https://wa.me/6281298830776" target="_blank" class="btn btn-whatsapp btn-lg rounded-pill shadow"><i class="bi bi-whatsapp"></i> Saya Tertarik</a>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="home-1">
+                            <img src="assets/img/home/home3.jpeg">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="section-4 py-6 bg-white">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        <h1 class="title mb-5">Cicilan hanya 4 juta-an / bulan<br>#clusterberkelas</h1>
+                        <a href="https://wa.me/6281298830776" target="_blank" class="btn btn-whatsapp btn-lg rounded-pill shadow"><i class="bi bi-whatsapp"></i> Hubungi Kami Segera</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="section-5 py-6">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="title text-center mb-5">Pernahkah Anda mengalami hal seperti ini?</h1>
+                        <div class="d-flex justify-content-center">
+                            <ul>
+                                <li>Numpang di rumah mertua, ngga bebas mau ngapain aja.</li>
+                                <li>Bayar sewa bulanan, tapi naikin harga seenaknya…</li>
+                                <li>Pengen renovasi rumah sesuai keinginan, tapi bukan punya sendiri.</li>
+                                <li>Sewa rumah bocor, pemilik tidak mau memperbaiki…</li>
+                                <li>Mau beli rumah murah, bingung prosesnya dan takut cicilan mahal!!!</li>
+                            </ul>
+                        </div>
+
+                        <div class="row justify-content-center my-5">
+                            <div class="col-lg-6">
+                                <img src="assets/img/component/pusing.jpg" class="rounded shadow-lg" width="100%">
+                            </div>
+                        </div>
+
+                        <h1 class="title text-center mb-5">CLUSTER DE' LEUWI SOLUSINYA!!!</h1>
+                        <h3 class="text-1 text-thin text-center">Kami siap membantu memiliki rumah impian Anda.
+                            Bayar sewa buat kaya pemilik rumah, bayar cicilan memperkaya diri sendiri !!!</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="section-6 bg-white py-6">
+            <div class="container">
+                <h1 class="title text-center mb-5">5 Keuntungan Utama Memiliki Rumah di cluster de' Leuwi</h1>
+
+                <div class="row justify-content-center text-center g-0 g-md-5 g-lg-5">
+                    <div class="col-md-6 col-lg-4 mb-5 mb-md-0 mb-lg-0">
+                        <img src="assets/icon/map.png" class="img-fluid mb-4" width="80">
+                        <h5 class="text-thin">Lokasi strategis nan asri di kota Depok.</h5>
+                    </div>
+                    <div class="col-md-6 col-lg-4 mb-5 mb-md-0 mb-lg-0">
+                        <img src="assets/icon/save-money.png" class="img-fluid mb-4" width="80">
+                        <h5 class="text-thin">Investasi yang sangat tepat, di harga launching sekarang, akan naik berkali lipat pada tahun berikutnya.</h5>
+                    </div>
+                    <div class="col-md-6 col-lg-4 mb-5 mb-md-0 mb-lg-0">
+                        <img src="assets/icon/discount.png" class="img-fluid mb-4" width="80">
+                        <h5 class="text-thin">Promo fantastis bunga KPR 4,5% tanpa pengajuan.</h5>
+                    </div>
+                    <div class="col-md-6 col-lg-4 mb-5 mb-md-0 mb-lg-0">
+                        <img src="assets/icon/star.png" class="img-fluid mb-4" width="80">
+                        <h5 class="text-thin">Bahan bangunan dan asesoris yang digunakan adalah kualitas premium.</h5>
+                    </div>
+                    <div class="col-md-6 col-lg-4 mb-5 mb-md-0 mb-lg-0">
+                        <img src="assets/icon/house.png" class="img-fluid mb-4" width="80">
+                        <h5 class="text-thin">Ready stock. Tidak perlu renovasi lagi, siap untuk langsung ditinggali.</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="section-7 py-6">
+            <div class="container">
+                <div class="maps mb-6">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.9774180794807!2d106.91197231427054!3d-6.396911464344911!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6995ab6d7a8247%3A0x87bcb2e5cd8d1ea2!2scluster%20de%E2%80%99%20Leuwi!5e0!3m2!1sid!2sid!4v1637138991158!5m2!1sid!2sid" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                </div>
+                <div class="text-center">
+                    <a href="https://goo.gl/maps/vNrbLxsoZACZ5MvY9" class="btn btn-gmaps btn-lg rounded-pill shadow">
+                        <img src="assets/icon/google-maps.png" width="20">
+                        Kunjungi Lokasi Cluster Kami
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="section-8 py-6 bg-white">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        <h1 class="title mb-5">Cicilan hanya 100rb-an / hari</h1>
+                        <a href="https://wa.me/6281298830776" target="_blank" class="btn btn-whatsapp btn-lg rounded-pill shadow"><i class="bi bi-whatsapp"></i> Saya Tertarik</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="section-9 py-6">
+            <div class="container">
+                <h1 class="title text-center mb-6">Foto Rumah</h1>
+
+                <div class="row g-3 mb-6" id="gallery">
+                    <div class="col-md-6 col-lg-6">
+                        <a href="assets/img/gallery/home1-3d.jpeg" class="fresco" data-fresco-group="gallery">
+                            <img src="assets/img/gallery/home1-3d.jpeg" class="img-fluid rounded shadow">
+                        </a>
+                    </div>
+                    <div class="col-md-6 col-lg-6">
+                        <a href="assets/img/gallery/home2-3d.jpeg" class="fresco" data-fresco-group="gallery">
+                            <img src="assets/img/gallery/home2-3d.jpeg" class="img-fluid rounded shadow">
+                        </a>
+                    </div>
+                    <div class="col-md-6 col-lg-6">
+                        <a href="assets/img/gallery/home1-rl.jpeg" class="fresco" data-fresco-group="gallery">
+                            <img src="assets/img/gallery/home1-rl.jpeg" class="img-fluid rounded shadow">
+                        </a>
+                    </div>
+                    <div class="col-md-6 col-lg-6">
+                        <a href="assets/img/gallery/home2-rl.jpeg" class="fresco" data-fresco-group="gallery">
+                            <img src="assets/img/gallery/home2-rl.jpeg" class="img-fluid rounded shadow">
+                        </a>
+                    </div>
+                    <div class="col-md-6 col-lg-6">
+                        <a href="assets/img/gallery/home12lt-3d.jpeg" class="fresco" data-fresco-group="gallery">
+                            <img src="assets/img/gallery/home12lt-3d.jpeg" class="img-fluid rounded shadow">
+                        </a>
+                    </div>
+                    <div class="col-md-6 col-lg-6">
+                        <a href="assets/img/gallery/home22lt-3d.jpeg" class="fresco" data-fresco-group="gallery">
+                            <img src="assets/img/gallery/home22lt-3d.jpeg" class="img-fluid rounded shadow">
+                        </a>
+                    </div>
+                    <div class="col-md-6 col-lg-6">
+                        <a href="assets/img/gallery/siteplane-3d.jpeg" class="fresco" data-fresco-group="gallery">
+                            <img src="assets/img/gallery/siteplane-3d.jpeg" class="img-fluid rounded shadow">
+                        </a>
+                    </div>
+                    <div class="col-md-6 col-lg-6">
+                        <a href="assets/img/gallery/siteplane-2d.jpeg" class="fresco" data-fresco-group="gallery">
+                            <img src="assets/img/gallery/siteplane-2d.jpeg" class="img-fluid rounded shadow">
+                        </a>
+                    </div>
+                </div>
+
+                <div class="text-center">
+                    <a href="https://wa.me/6281298830776" target="_blank" class="btn btn-whatsapp btn-lg rounded-pill shadow"><i class="bi bi-whatsapp"></i> Hubungi Kami Segera</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <footer class="bg-dark text-white text-center py-3">
+        <div class="container">
+            <small>Disclaimer: Semua informasi dalam website ini adalah tidak mengikat dalam Perjanjian Jual
+                Beli</small>
+        </div>
+    </footer>
+
+    <!-- Jquery -->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+
+    <!-- Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Fresco -->
+    <script src="/assets/vendor/fresco/dist/js/fresco.min.js"></script>
+</body>
+
+</html>
